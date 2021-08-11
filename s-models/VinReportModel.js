22583ad7b2d8034f59593b463d4a71db06ac5ef8
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-11 01:20:16 -0400

    Improved

--- s-models/VinReportModel.js
+++ s-models/VinReportModel.js
@@ -11,6 +11,41 @@ module.exports = mongoose.model(
 			type: String,
 		},
 
+		vinData: {
+			is_salvaged: {
+				type: Boolean,
+				required: true,
+				default: true,
+			},
+
+			info: {
+				vehicle_title: {
+					type: String,
+					maxlength: 100,
+				},
+
+				loss_type: {
+					type: String,
+					maxlength: 100,
+				},
+
+				mileage: {
+					type: String,
+					maxlength: 100,
+				},
+
+				primary_damage: {
+					type: String,
+					maxlength: 100,
+				},
+
+				secondary_damage: {
+					type: String,
+					maxlength: 100,
+				},
+			}
+		},
+
 		created_at: {
 			type: Date,
 			default: Date.now,
