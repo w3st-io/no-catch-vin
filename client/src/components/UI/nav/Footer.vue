cd5ce669ba7c6bfdcce4ce117629412f78a2dd19
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-14 03:16:38 -0400

    Improved

--- client/src/components/UI/nav/Footer.vue
+++ client/src/components/UI/nav/Footer.vue
@@ -1,22 +1,33 @@
 <template>
 	<!-- Footer -->
 	<footer class="pt-4 page-footer font-small bg-dark text-center">
-		<BButton
-			v-if="$store.state.userLogged"
-			variant="outline-secondary"
-			size="sm"
-			class="m-auto"
-			@click="logout()"
-		>Log Out</BButton>
+		<BRow>
+			<BCol cols="12" md="6">
+				<RouterLink to="/legal">Legal Disclaimer</RouterLink>
+			</BCol>
 
-		<!-- Copyright -->
-		<div class="pt-3 pb-5 footer-copyright text-secondary">
-			<a href="https://w3st.io/"></a>
-			© {{ new Date().getFullYear() }}
-			<br>
-			<a href="https://iexcloud.io">Data provided by IEX Cloud</a>
-			<h6><i class="text-dark">"Dare I say it..?"</i></h6>
-		</div>
+			<BCol cols="12" md="6">
+				<!-- [USER] -->
+				<BButton
+					v-if="$store.state.userLogged"
+					variant="outline-secondary"
+					size="sm"
+					class="m-auto"
+					@click="logout()"
+				>Log Out</BButton>
+			</BCol>
+
+			<!-- Copyright -->
+			<BCol cols="12">
+				<div class="pt-3 pb-5 footer-copyright text-secondary">
+					<a href="https://w3st.io/"></a>
+					© {{ new Date().getFullYear() }}
+					<br>
+					<a href="https://iexcloud.io">Data provided by IEX Cloud</a>
+					<h6><i class="text-dark">"Dare I say it..?"</i></h6>
+				</div>
+			</BCol>
+		</BRow>
 	</footer>
 </template>
 
