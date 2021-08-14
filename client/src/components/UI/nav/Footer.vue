c2846c4b0a5932e0521d71a9ae876ee3c0a6aad0
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-14 03:19:09 -0400

    Imprvoed

--- client/src/components/UI/nav/Footer.vue
+++ client/src/components/UI/nav/Footer.vue
@@ -1,33 +1,34 @@
 <template>
 	<!-- Footer -->
-	<footer class="pt-4 page-footer font-small bg-dark text-center">
-		<BRow>
-			<BCol cols="12" md="6">
-				<RouterLink to="/legal">Legal Disclaimer</RouterLink>
-			</BCol>
+	<footer class="page-footer pt-5 text-center font-small bg-dark">
+		<BContainer fluid>
+			<BRow clas="w-100">
+				<BCol cols="12" md="6">
+					<!-- [USER] -->
+					<BButton
+						v-if="$store.state.userLogged"
+						variant="outline-secondary"
+						size="sm"
+						@click="logout()"
+					>Log Out</BButton>
+				</BCol>
 
-			<BCol cols="12" md="6">
-				<!-- [USER] -->
-				<BButton
-					v-if="$store.state.userLogged"
-					variant="outline-secondary"
-					size="sm"
-					class="m-auto"
-					@click="logout()"
-				>Log Out</BButton>
-			</BCol>
+				<BCol cols="12" md="6">
+					<RouterLink to="/legal">Legal Disclaimer</RouterLink>
+				</BCol>
 
-			<!-- Copyright -->
-			<BCol cols="12">
-				<div class="pt-3 pb-5 footer-copyright text-secondary">
-					<a href="https://w3st.io/"></a>
-					© {{ new Date().getFullYear() }}
-					<br>
-					<a href="https://iexcloud.io">Data provided by IEX Cloud</a>
-					<h6><i class="text-dark">"Dare I say it..?"</i></h6>
-				</div>
-			</BCol>
-		</BRow>
+				<!-- Copyright -->
+				<BCol cols="12">
+					<div class="pt-3 pb-5 footer-copyright text-secondary">
+						<a href="https://w3st.io/"></a>
+						© {{ new Date().getFullYear() }}
+						<br>
+						<a href="https://iexcloud.io">Data provided by IEX Cloud</a>
+						<h6><i class="text-dark">"Dare I say it..?"</i></h6>
+					</div>
+				</BCol>
+			</BRow>
+		</BContainer>
 	</footer>
 </template>
 
